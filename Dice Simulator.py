39a29533e6845cda90ea69c2260f9b723650c80d
amirhoseinsohrabi <amirhoseinsohrabi.official@gmail.com>
2022-04-24 21:23:02 +0430

    add dice

---
+++ Dice Simulator.py
@@ -0,0 +1,13 @@
+import random
+
+def dice(input_):
+        input_ = int(input_)
+        test = random.randint(1,6)
+        for i in range(input_):
+            test
+            i+=1
+            if i == input_:
+                print(test)
+
+while True:
+    dice(input("Please Enter Number of dice rolled : "))
